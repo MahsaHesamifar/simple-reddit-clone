71c6e0888e30924067f16c3190e183bf80e2b10e
Mahsa <m.hesamifar@yahoo.com>
2024-05-30 12:03:36 +0330

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,17 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatars.githubusercontent.com",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
